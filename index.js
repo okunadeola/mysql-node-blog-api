a8408e781c3a76a1e160106ddd9f39b00baec964
okunade olaitan <olaokunade@gmail.com>
2022-10-09 10:08:54 +0100

    blog initial commit edit

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import { db } from "./db.js";
+import dotenv from 'dotenv'
+
+
 
 const app = express();
 
 
 
+dotenv.config();
 app.use(cors({ origin: true, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
@@ -27,7 +31,8 @@ db.connect(function(err) {
 
   console.log('Connected to the MySQL server.');
 });
+const PORT = process.env.PORT || 8800
 
-app.listen(8800, () => {
+app.listen(PORT, () => {
   console.log("Connected!");
 });
