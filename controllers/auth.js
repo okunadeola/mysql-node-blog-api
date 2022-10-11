2a8e2d578f023de63b68095942084abce4b169d4
okunade olaitan <olaokunade@gmail.com>
2022-10-11 13:03:22 +0100

    latest

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,5 @@ export const login = (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("access_token",{
-    sameSite:"none",
-    secure:true
-  }).status(200).json("User has been logged out.")
+  res.status(200).json("User has been logged out.")
 };
